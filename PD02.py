85f3bbc4e746436a545b192e42a1cadd60f7c0f1
zimelemonta <ziemelemonta@gmail.com>
2024-12-13 12:15:46 +0200

    Add files via upload

---
+++ PD02.py
@@ -0,0 +1,13 @@
+#2 uzdevums
+n=int(input("ievadi skaitli n:"))
+summa= sum (range(1, n+1))
+print()
+n=5
+
+# 3 uzdevums
+import.math
+n = int(input("ievadi skaitli n:"))
+print("!") = (factorial)
+n=5
+
+#4 uzdevums
